5bce40ac60eb6cf68ab8c75ee961556531cd806b
NileRiva <niletributary@gmail.com>
2023-02-03 14:52:11 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "https://ecse-week3-demo.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+fake_database = []
+
+@app.get("/todos")
+async def get_all_todos():
+  return fake_database
+
+@app.post("/todos")
+async def create_todo(request: Request):
+  todo = await request.json()
+  fake_database.append(todo)
+  return todo
+
+@app.delete("/todos/{id}")
+async def delete_todo_by_id(id:int):
+    for todo in fake_database:
+      if todo["id"]==id:
+        print("Retrieved")
+        fake_database.remove(todo)
+        break
+
+@app.patch("/todos/{id}")
+async def update_todo_by_id(id: int,request:Request):
+    todoupdate = await request.json()
+        #if todoupdate["id"]!=Null:
+    i=0
+    for todo in fake_database:
+        if todo["id"]==id:
+            fake_database[i]=todoupdate
+        i+=1
+    return 
